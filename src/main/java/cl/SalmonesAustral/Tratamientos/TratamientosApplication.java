5df53e91c7f64a4306c93bec5e2446e667616c23
Crainsver <cristobal21rc@gmail.com>
2026-05-29 10:41:47 -0400

    MicroservicioTratemiento-commit

---
+++ src/main/java/cl/SalmonesAustral/Tratamientos/TratamientosApplication.java
@@ -0,0 +1,13 @@
+package cl.SalmonesAustral.Tratamientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TratamientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TratamientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/SalmonesAustral/Tratamientos/TratamientosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.SalmonesAustral.Tratamientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TratamientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
